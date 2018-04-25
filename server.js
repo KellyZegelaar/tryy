f1d37cd80d55b64aee66a66bf0fa246ca2d64e08
Kelly <knzegela@avans.nl>
2018-04-25 14:03:49 +0200

    changed port

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/hello', (req, res) => {
     res.json({'msg':'Hello mofo'})
 });
 
-app.listen(8081, ()=> {
+const port = process.env.PORT || 8080;
+
+app.listen(port, ()=> {
     console.log('server is ready')
 });
